d5c0477a4b5829efafd4487652063b535b351955
btsybm <betsybeam2@gmail.com>
2021-07-03 00:53:28 -0500

    Buttons value stored in array when clicked until array is at 4.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,51 @@
-console.log('test');
+
+/*-------------------------------- Constants --------------------------------*/
+
+// Combo for each combo of answers (shorter way)
 
 
+  /*---------------------------- Variables (state) ----------------------------*/
 
-// Make constants for each combo of answers 
-// -	(is there short way to do this)
-// Objects for the four questions/choices {Question, button1, button2, button3}
-// Variable for scoreArray
-// 
-// Cache paragraph and buttons
-//
-// Set up event listener for all three buttons.
-// 
-// Create a function that will log which button is clicked by putting that buttons value into a scoreArray [q1, q2, q3, q4] ex. [1,1,3,2]
+let choices = []; //array with log all four clicks 
 
-// Create a function that will replace the cached paragraph and buttons with the question/choices from the 2nd object in the array (then 3 then 4).  Stop at four clicks (until scoreArray is full and loop has gone through entire questions array) then calls function below
+  
+  /*------------------------ Cached Element References ------------------------*/
+  
+let firstButtton = document.querySelector("#b1")
+let secondButtton = document.querySelector("#b2")
+let thirdButtton = document.querySelector("#b3")
+  
+  /*----------------------------- Event Listeners -----------------------------*/
+  
 
-// Create function that takes the full scoreArray, filters it through answer combos, and returns one of the shows assigned to the matching combo. (Title goes into <p> and image goes where the buttons were)
-//    Needs to remove buttons and replace with div / img box
-//    Add button below image that will return another option from the same answer combo, endlessly
-//    TBD - make arrays with results for each combo of answers or find a way to have each scoreArray pull randomly from an API
+firstButtton.addEventListener("click", function() {
+  if (choices.length < 4) {
+  choices.push(1);
+  // nextQuestion()   ****should work kind of like render did in TTT
+  console.log("logged 1")}
+    // else {getRec()}
+})
 
-// After game works:
-// Add sound and maybe a timer
-// Change background image after each click to reflect choice from previous question
-// Delay click function to slightly slow down how quickly the next question pops up
-// Add images to buttons
+secondButtton.addEventListener("click", function() {
+  if (choices.length < 4) {
+    choices.push(2)
+    console.log("logged 2")
+  }})
+
+thirdButtton.addEventListener("click", function() {
+  if (choices.length < 4) {
+    choices.push(3)
+    console.log("logged 3")
+  }})
+
+
+  /*-------------------------------- Functions --------------------------------*/
+
+
+//function nextQuestion () {
+  // If(choices.length=1) **after push above** --- switch out h2/buttons with question 2 and options
+      // else if(choices.length=2) **after push above** --- switch out h2/buttons with question 3 and options
+      // else if(choices.length=3) **after push above** --- switch out h2/buttons with question 4 and options
+//}
+
+//function getRec () {}  *******how to use the completed choices array to pull from API
