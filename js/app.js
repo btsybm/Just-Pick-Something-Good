c448379fae7c6507778002e46d1d273add6a64fe
btsybm <betsybeam2@gmail.com>
2021-07-05 16:36:36 -0500

    After last choice buttons are removed and an image replaces them

--- js/app.js
+++ js/app.js
@@ -43,25 +43,32 @@ function nextQuestion() {
   if (choices.length < 2) { 
     document.getElementById("b1").value = "COMEDY";
     document.getElementById("b2").value = "DRAMA";
-    document.getElementById("b3").innerHTML = "EITHER";
-    document.getElementById("mainText").value = "Genre"
+    document.getElementById("b3").value = "EITHER";
+    document.getElementById("mainText").innerHTML = "Genre"
 
     } else if (choices.length < 3) {
       document.getElementById("b1").value = "LEAD";
       document.getElementById("b2").value = "SUPPORTING";
-      document.getElementById("b3").innerHTML = "EITHER";
-      document.getElementById("mainText").value = "HOW PROMINANT"
+      document.getElementById("b3").value = "EITHER";
+      document.getElementById("mainText").innerHTML = "HOW PROMINANT"
     } else if (choices.length < 4) {
       document.getElementById("b1").value = "NEW";
       document.getElementById("b2").value = "OLD";
-      document.getElementById("b3").innerHTML = "EITHER";
-      document.getElementById("mainText").value = "NEW OR OLD"
+      document.getElementById("b3").value = "EITHER";
+      document.getElementById("mainText").innerHTML = "NEW OR OLD";
+    
+    
+    } else if (choices.length < 5) {
+      document.getElementById("buttons").remove();
+      document.getElementById("mainText").innerHTML = "YOU SHOULD WATCH:";
+      document.getElementById("finalImage").src = "/images/blackwidow.jpeg";
+      }
+
     // } else {
       //  function getRec()
     //   }  // take choices array and return a suggestions
     
-
-  }}
+  }
 
 
 
